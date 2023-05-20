342ec5473d89cc45f03deb3afb9d9f9e80afbc17
cmd69 <al386160@uji.es>
2023-05-20 15:20:42 +0200

    v and prod envoironments setup

--- settings/configDev.py
+++ settings/configDev.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 # Load values from .env file
 load_dotenv()
-
+DEBUG = True
 DEV_DB_PATH = os.environ.get('DEV_DB_PATH')
 DEV_BOT_TOKEN = os.environ.get('DEV_BOT_TOKEN')
--- settings/configProd.py
+++ settings/configProd.py
@@ -1,2 +1,8 @@
-DATABASE_PATH = 
-BOT_KEY =
+import os
+from dotenv import load_dotenv
+
+# Load values from .env file
+load_dotenv()
+DEBUG = False
+PROD_DB_PATH = os.environ.get('PROD_DB_PATH')
+PROD_BOT_TOKEN = os.environ.get('PROD_BOT_TOKEN')
