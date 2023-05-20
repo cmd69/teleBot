c7fde442ac17fa0a04a49789e151132b22f91a5e
cmd69 <al386160@uji.es>
2023-05-20 17:02:05 +0200

    States, Keyboards and /start added

--- settings/configProd.py
+++ settings/configProd.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 # Load values from .env file
 load_dotenv()
 DEBUG = False
-PROD_DB_PATH = os.environ.get('PROD_DB_PATH')
-PROD_BOT_TOKEN = os.environ.get('PROD_BOT_TOKEN')
+DB_PATH = os.environ.get('PROD_DB_PATH')
+BOT_TOKEN = os.environ.get('PROD_BOT_TOKEN')
