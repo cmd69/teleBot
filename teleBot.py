5d4db50e7adf7e1027366c6510af0de965b5d1d8
cmd69 <al386160@uji.es>
2023-05-20 15:20:42 +0200

    v and prod envoironments setup

--- teleBot.py
+++ teleBot.py
@@ -1,10 +1,23 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
+import importlib
 
 app = Flask(__name__)
+
+mode = os.environ.get('TELEBOT_ENV', 'development')  # Default to development mode if not specified
+
+if mode == 'production':
+    app.config.from_pyfile('settings/configProd.py')
+else:
+    app.config.from_pyfile('settings/configDev.py')
+
 
 
 @app.route('/')
 def hello():
     return 'Hello, world!'
+
+
+if __name__ == '__main__':
+    app.run()
