1d3343daee7f2649386d189e40009e677054b438
gitCMD <nasamigra@gmail.com>
2023-06-13 22:48:45 +0200

    switched to flask with streamlit

--- telebot.py
+++ telebot.py
@@ -1,31 +1,42 @@
 from aiogram import executor
-
 # Local Imports
-from flask_app import setup_flask, run_flask
-from bot_setup import setup_bot
-
+from bot_setup import setup_bot, setup_config
+from multiprocessing import Process
+import subprocess
 
 
 # Setup Flask
-app = setup_flask()
+app_config = setup_config()
 
 # Setup Bot
-bot, dp, dbManager, keyboardFactory, tablesFactory, chartsGenerator = setup_bot(app)
+bot, dp, dbManager, keyboardFactory, tablesFactory, chartsGenerator = setup_bot(app_config)
 
-run_flask(app)
 
-@app.route('/test', methods=['GET'])
-def handle_payload():
-    return 'test'
+def run_bot():
+
+    
+    executor.start_polling(dp, skip_updates=True)
+
+
+def run_streamlit():
+    app_path = "streamlit_app.py"
+    subprocess.Popen(["streamlit", "run", app_path])
+
 
 if __name__ == "__main__":
-    
-    
-    # Import handlers after creating the Dispatcher
     from handlers.main_handler import *
     from handlers.add_expense_handler import *
     from handlers.add_income_handler import *
     from handlers.fetch_month_handler import *
     from handlers.fetch_all_handler import *
+    bot_process = Process(target=run_bot)
+    streamlit_process = Process(target=run_streamlit)
+
+    bot_process.start()
+    streamlit_process.start()
+
+    bot_process.join()
+    streamlit_process.join()
     
-    executor.start_polling(dp, skip_updates=True)
+    # Import handlers after creating the Dispatcher
+    
